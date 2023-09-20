b9c7fe9b575d8618e07ba0b9a133befe0fe81312
OMURA Shinichi <song.of.sand@gmail.com>
2023-09-20 18:42:02 +0900

    remove a tag with a comment line 	modified:   index.html 	modified:   make-list.sh

--- docs/make-list.sh
+++ docs/make-list.sh
@@ -1,2 +1,7 @@
-awk 'BEGIN { FS = "," } { print "<a href=" $2 "> "  $1 "</a><br/>"  }' desc.csv
+awk 'BEGIN { FS = "," } { if ( $2 ~ /pdf/ ) 
+print "<a href=" $2 "> "  $1 "</a><br/>" 
+else 
+print $1 "<br/>"
+ }' desc.csv
+
 
